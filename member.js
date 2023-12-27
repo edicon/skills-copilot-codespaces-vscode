f14064b5e5c267eb28754660dcc369307346fe19
EDICON <hslee.edicon@gmail.com>
2023-12-27 00:01:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberInfo = document.getElementById("memberInfo");
+    var skillsInfo = document.getElementById("skillsInfo");
+    var projectsInfo = document.getElementById("projectsInfo");
+    var contactInfo = document.getElementById("contactInfo");
+    member.style.backgroundColor = "#f5f5f5";
+    skills.style.backgroundColor = "#e9ecef";
+    projects.style.backgroundColor = "#e9ecef";
+    contact.style.backgroundColor = "#e9ecef";
+    memberInfo.style.display = "block";
+    skillsInfo.style.display = "none";
+    projectsInfo.style.display = "none";
+    contactInfo.style.display = "none";
+}
